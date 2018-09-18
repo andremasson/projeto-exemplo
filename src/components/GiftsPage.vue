e893396ff034f69579c6eaba3c2d6221a96b3ea5
André Masson <masson.andre@gmail.com>
2018-09-17 23:44:14 -0300

    Dados da conta; fotos presentes

--- src/components/GiftsPage.vue
+++ src/components/GiftsPage.vue
@@ -24,8 +24,27 @@
       <b-modal ref="modalgift" size="lg" ok-only title="Eba!" @ok="save">
         <h3>{{list[selectedIndex].title}}</h3>
         <p>Para contribuir para este presente, pedimos que deposite na nossa conta.</p>
-        <p>[dados da conta...]</p>
-        <p>Nos fale também quem é você [frase melhor...]</p>
+        <b-row class="dados-conta">
+          <b-col>
+            <p>Banco do Brasil</p>
+            <p>Agência: 4093-3</p>
+            <p>Conta corrente: 71932-3</p>
+            <p>CNPJ: 18.763.130/0001-41</p>
+            <p>Roger Ferreira Batista</p>
+          </b-col>
+          <b-col id="coluna-meio" style="text-align: center; horizontal-align: middle; margin: auto auto auto">
+            <p>Ou</p>
+          </b-col>
+          <b-col>
+            <p>Itaú</p>
+            <p>Agência: 0185</p>
+            <p>Conta corrente: 09731-8</p>
+            <p>CPF: 343.250.488-80</p>
+            <p>Susana Miki Nagae</p>
+          </b-col>
+        </b-row>
+        <br/>
+        <p>Nos fale também quem é você...</p>
         <div class="form-group has-feedback">
           <label for="name" class="sr-only">Nome completo</label>
           <input v-model="name" class="form-control" placeholder="Nome completo">
@@ -86,79 +105,79 @@ export default {
             title: "Drinks em Whistler",
             value: "(CAD 27,00 / R$ 80,00)",
             contribute: "R$40",
-            image: require('@/assets/images/danca.jpg')
+            image: require('@/assets/images/01.jpg')
           },
           {
             title: "VanDusen Festival of Lights",
             value: "(CAD 38,00 ou R$ 114,00)",
             contribute: "R$57",
-            image: require('@/assets/images/inicio.jpg')
+            image: require('@/assets/images/02.jpg')
           },
           {
             title: "Ice Skating in Whistler",
             value: "(CAD 50,00 ou R$ 150,00)",
             contribute: "R$75",
-            image: require('@/assets/images/hopaholics.jpg')
+            image: require('@/assets/images/03.jpg')
           },
           {
             title: "Jantar no Shabusen Yakiniku",
             value: "(CAD 70,00 ou R$ 210,00)",
             contribute: "R$105",
-            image: require('@/assets/images/danca.jpg')
+            image: require('@/assets/images/04.jpg')
           },
           {
             title: "Brunch  no Purebread",
             value: "(CAD 80,00 / R$ 240,00)",
             contribute: "R$120",
-            image: require('@/assets/images/inicio.jpg')
+            image: require('@/assets/images/05.jpg')
           },
           {
             title: "Capilano Suspension Bridge park",
             value: "(CAD 94,00 ou R$ 282,00)",
             contribute: "R$141",
-            image: require('@/assets/images/hopaholics.jpg')
+            image: require('@/assets/images/06.jpg')
           },
           {
             title: "Peak 2 Peak Gondola",
             value: "(CAD 110,00 / R$ 330,00)",
             contribute: "R$165",
-            image: require('@/assets/images/danca.jpg')
+            image: require('@/assets/images/07.jpg')
           },
           {
             title: "Jantar romântico",
             value: "(CAD 150,00 / R$ 450,00)",
             contribute: "R$225",
-            image: require('@/assets/images/inicio.jpg')
+            image: require('@/assets/images/08.jpg')
           },
           {
             title: "Scandinave Spa Whistler",
             value: "(CAD 158,00 ou R$ 474,00)",
             contribute: "R$237",
-            image: require('@/assets/images/hopaholics.jpg')
+            image: require('@/assets/images/09.jpg')
           },
           {
             title: "Esquiar",
             value: "(CAD 160,00 / R$ 480,00)",
             contribute: "R$240",
-            image: require('@/assets/images/danca.jpg')
+            image: require('@/assets/images/10.jpg')
           },
           {
             title: "Diária do Blackcomb Lodge",
             value: "(CAD 167,00 / R$ 500,00)",
             contribute: "R$250",
-            image: require('@/assets/images/inicio.jpg')
+            image: require('@/assets/images/11.jpg')
           },
           {
             title: "Super Fly - Zipline & treetop adventures",
             value: "(CAD 180,00 / R$ 540,00)",
             contribute: "R$270",
-            image: require('@/assets/images/hopaholics.jpg')
+            image: require('@/assets/images/12.jpg')
           },
           {
             title: "Passagens aéreas",
             value: "(R$ 4145,00)",
             contribute: "R$2072",
-            image: require('@/assets/images/danca.jpg')
+            image: require('@/assets/images/13.jpg')
           }
         ],
         images: {
@@ -173,6 +192,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="css">
+.dados-conta p {
+  -webkit-margin-before: 0px;
+  -webkit-margin-after: 0px;
+}
+
 #title-row {
   padding-top: 5em;
 }
@@ -188,6 +212,12 @@ export default {
 
 .showcase .row {
   width: -webkit-fill-available;
+}
+
+@media (max-width: 767px) {
+  #coluna-meio p {
+    margin: 2em 5em 1em 5em;
+  }
 }
 
 @media (min-width: 768px) {
